7c4f851e03dfe32eb9922233c9a75f507c33a80e
kazarian <michael.kazarian@gmail.com>
2019-10-21 21:19:54 +0300

    kbd.el was updated

--- rc/kbd.el
+++ rc/kbd.el
@@ -18,15 +18,19 @@
 (global-set-key (kbd"C--") 'hs-hide-block)
 (global-set-key (kbd"C-0") 'hs-show-all)
 (global-set-key (kbd"C-9") 'hs-hide-all)
-;; Russian keys
+;; Cyrillic keys
 (global-set-key [?\C-ч ?\C-ы] 'save-buffer)     ; C-x C-s
 (global-set-key [?\C-ч ?\C-і] 'save-buffer)     ; C-x C-s
+(global-set-key [?\C-ч ?\C-и] 'ibuffer)         ; C-x C-b
+(global-set-key [?\C-ч ?\и] 'iswitchb-buffer)   ; C-x b
+(global-set-key [?\C-ч ?\щ] 'other-window)      ; C-x o
 (global-set-key [?\C-я] 'undo) ;C-z
 (global-set-key [?\M-я] 'redo) ;M-z
+(global-set-key [?\C-п] 'keyboard-quit)   ; C-g
 (global-set-key [?\C-і] 'isearch-forward) ;C-s
 (global-set-key [?\C-ы] 'isearch-forward) ;C-s
 (global-set-key [?\C-к] 'isearch-backward) ;C-r
-(global-set-key [?\M-ц] 'copy-region-as-kill-nomark) ; M-w
+(global-set-key [?\M-ц] 'kill-ring-save) ; M-w
 (global-set-key [?\C-ц] 'kill-region)      ; C-w
 (global-set-key [?\C-н] 'yank)      ; C-y
 (global-set-key [?\C-е] 'move-end-of-line)      ; C-e
