813ee65dd1620cef31f948e26c2a5d7a38a8e4d1
kazarian <michael.kazarian@gmail.com>
2019-10-22 11:07:18 +0300

    rc-json was added

---
+++ rc/rc-json.el
@@ -0,0 +1,22 @@
+;;; rc-json.el --- 
+
+;; Copyright (C) Michael Kazarian
+;;
+;; Author: Michael Kazarian <michael.kazarian@gmail.com>
+;; Keywords: 
+;; Requirements: 
+;; Status: not intended to be distributed yet
+
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)
+            (display-line-numbers-mode)
+            (highlight-indentation-mode)
+            (whitespace-mode 1)
+            (local-set-key (kbd "C-;") 'comment-or-uncomment-region)
+            (electric-pair-mode)
+            (local-set-key [f8] 'flycheck-mode)
+            ))
+
+;;; rc-json.el ends here
