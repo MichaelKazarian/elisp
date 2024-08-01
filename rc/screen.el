0b5ab5b817ba2c2ae7b510b5088d40674e90113b
Kazarian Mykhailo <michael.kazarian@gmail.com>
2024-08-01 12:17:33 +0300

    Theme update

--- rc/screen.el
+++ rc/screen.el
@@ -44,8 +44,8 @@
 
 ;; Color theme
 (setq host (system-name))
-(cond ((member host '("asm-3" "rpi5-mk")) (load-theme 'kaolin-mono-light t))
-      ((equal host "rpzero2") (load-theme 'kaolin-eclipse t))
+(cond ((not (display-graphic-p)) (load-theme 'tango-dark t))
+      ((member host '("asm-3" "rpi5-mk" "rpzero2")) (load-theme 'kaolin-mono-light t))
       ((equal host "mac-asm3.local") (load-theme 'kaolin-breeze t))
       (t (load-theme 'sunny-day t)))
 ;; (add-to-list 'custom-theme-load-path "~/elisp/rc/my-themes")
