c19081691385e8d6f6dd60e3d5d07ae11e84ff1f
Mykhailo Kazarian <michael.kazarian@gmail.com>
2025-08-14 17:15:36 +0300

    new order

--- rc/rc-dired.el
+++ rc/rc-dired.el
@@ -6,8 +6,8 @@
 (add-to-list 'load-path (expand-file-name "~/elisp/mode/dired-sort"))
 (add-to-list 'load-path (expand-file-name "~/elisp/mode/dired-lister"))
 (require 'dired-lister)
-(require 'dired-sort)
 (dired-lister-mode 1)
+(require 'dired-sort)
 (dired-sort-mode 1)
 
 ;;; rc-dired.el ends here
